079040037ffa9d86225fdaea58a3cdda2cd2d7c4
Max <pe.pe.l@yandex.ru>
2024-11-12 19:33:16 +0300

    Add routes for saving and getting message

--- src/one-time-link/one-time-link.controller.ts
+++ src/one-time-link/one-time-link.controller.ts
@@ -1,7 +1,44 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  GoneException,
+  NotFoundException,
+  Param,
+  ParseUUIDPipe,
+  Post,
+  Req,
+} from '@nestjs/common';
 import { OneTimeLinkService } from './one-time-link.service';
+import { Request } from 'express';
+import { CreateLinkDto } from './dto/create-link.dto';
 
 @Controller('one-time-link')
 export class OneTimeLinkController {
   constructor(private readonly oneTimeLinkService: OneTimeLinkService) {}
+
+  @Post()
+  async setLink(
+    @Req() request: Request,
+    @Body() dto: CreateLinkDto,
+  ): Promise<string> {
+    const { id } = await this.oneTimeLinkService.create(dto.message);
+    const link = `${request.protocol}://${request.headers.host}/one-time-link/${id}`;
+
+    return link;
+  }
+
+  @Get(':id')
+  async getMessage(
+    @Param('id', new ParseUUIDPipe()) id: string,
+  ): Promise<string> {
+    const link = await this.oneTimeLinkService.getById(id);
+
+    if (!link) throw new NotFoundException('Link was not found');
+    if (link.isUsed) throw new GoneException('This link has been used!');
+
+    await this.oneTimeLinkService.useLink(id);
+
+    return link.message;
+  }
 }
--- src/one-time-link/one-time-link.service.ts
+++ src/one-time-link/one-time-link.service.ts
@@ -1,4 +1,20 @@
 import { Injectable } from '@nestjs/common';
+import { Link } from '@prisma/client';
+import { PrismaService } from 'src/prisma/prisma.service';
 
 @Injectable()
-export class OneTimeLinkService {}
+export class OneTimeLinkService {
+  constructor(private prisma: PrismaService) {}
+
+  async getById(id: string): Promise<Link | null> {
+    return this.prisma.link.findUnique({ where: { id } });
+  }
+
+  async create(message: string): Promise<Link> {
+    return this.prisma.link.create({ data: { message } });
+  }
+
+  async useLink(id: string): Promise<Link> {
+    return this.prisma.link.update({ where: { id }, data: { isUsed: true } });
+  }
+}
